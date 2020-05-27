0a3f84d81dfd87dd3c84e08f00e1d25bdbb7e16a
baranetharen <44423440+baranetharen@users.noreply.github.com>
2020-05-26 23:46:55 -1200

    Update Program.cs

--- AESEncryptor/Program.cs
+++ AESEncryptor/Program.cs
@@ -7,9 +7,7 @@ using System.Threading.Tasks;
 namespace AESEncryptor
 {
     class Program
-    {
-      
-    
+    {    
         static void Main(string[] args)
         {
             CommandExecutor commandExecutor = new CommandExecutor();
